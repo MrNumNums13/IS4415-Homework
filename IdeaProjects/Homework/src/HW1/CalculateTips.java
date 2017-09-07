68a7e9f9370161f60c6dbf9746e6dbbab5b81707
Harry Hanskat <harryhanskat@me.com>
2017-09-07 10:04:28 -0600

    Hello World!

--- IdeaProjects/Homework/src/HW1/CalculateTips.java
+++ IdeaProjects/Homework/src/HW1/CalculateTips.java
@@ -1,7 +1,7 @@
 package HW1;
 
 public class CalculateTips {
-    public static void main(String [] args){
-        System.out.println("ehhlfadsuiopfhas");
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
     }
 }
--- IdeaProjects/Homework/src/com/HW1/Main.java
+++
@@ -1,8 +0,0 @@
-package com.HW1;
-
-public class Main {
-
-    public static void main(String[] args) {
-	 System.out.println("This is another test of the git push functionality");
-    }
-}
